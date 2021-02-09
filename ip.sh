f1caaff92f9dd61b80755b3ebe8b0efad3df8caa
Phone Metal <74254069+Phone-Metal@users.noreply.github.com>
2021-02-09 20:55:33 +0600

    Bug bashed

--- ip.sh
+++ ip.sh
@@ -27,8 +27,8 @@ sleep 1
  
 clear
 banner_func 
-	
-echo -e "Enter IP Address:"
+echo -e "" 	
+echo -e " \e[32mEnter IP Address:"
 #echo "\(Note: The Report Feature Is A Bit Buggy)"
 read ip_address 
 sleep 0.8
@@ -62,6 +62,7 @@ echo "Conducting Automated OSINT with ROBTEX FREE API:"
 #workaround because I'm lazy 
 #robtex
 sleep 2.1
+echo "Robtex Report:"
 curl  -sS https://freeapi.robtex.com/ipquery/"$ip_address" | jq | tr -d "}{[]"  
 curl  -sS https://freeapi.robtex.com/ipquery/"$ip_address" | jq | tr -d "}{[]" 2>/dev/null > ROBTEX_REPORT.txt
 #TC
@@ -69,6 +70,7 @@ sleep 1.9
 echo "" 
 echo "Conducting Automated OSINT with THREAT CROWD" 
 #curl https://www.threatcrowd.org/searchApi/v2/ip/report/?"$ip_address" | jq | tr -d "{}[]" 
+echo "Threat Crowd Report:" 
 curl  -sS https://www.threatcrowd.org/searchApi/v2/ip/report/?ip=$ip_address | jq | tr -d "{}[]" 
 curl  -sS https://www.threatcrowd.org/searchApi/v2/ip/report/?ip=$ip_address | jq | tr -d "{}[]" 2>/dev/null > THREATCROWD_REPORT.txt
 #call Function 
@@ -77,32 +79,42 @@ sleep 2.1
 ip_whois
 ip_whois 2>/dev/null > IPWHOIS_Report.txt  
 echo "Conducting Automated OSINT with ANY.GE API" 
-curl  -sS https://any.ge/api/ip/api.php?"$ip_address" | tr -d "{}"
-curl  -sS https://any.ge/api/ip/api.php?"$ip_address" | tr -d "{}" 2>/dev/null > ANYGE_report.txt
+echo "Any.ge Report:" 
+curl  -sS https://any.ge/api/ip/api.php?$ip_address | tr -d "{}"
+curl  -sS https://any.ge/api/ip/api.php?$ip_address | tr -d "{}" 2>/dev/null > ANYGE_report.txt
 
 # Google it
+echo "Conducting Automated OSINT with GOOGLE" 
 sleep 2
 echo "" 
 gvar="https://www.google.com/search?&ie=UTF-8&oe=UTF-8&q=intext:%$ip_address" 
-echo "Conducting Automated OSINT with GOOGLE" 
 echo "URL : $gvar"  
-
+#DShield
+echo "Conducting Automated OSINT with DSHIELD API" 
+sleep 0.9
+echo "DShield API Report:" 
+curl -sS http://isc.sans.edu/api/ip/$ip_address | tr -d "{}" | jq 
+curl -sS https://isc.sans.edu/api/ipdetails/$ip_address | tr -d "{}" | jq
+curl -sS http://isc.sans.edu/api/ip/$ip_address | tr -d "{}" | jq 2>/dev/null > DH_report.txt
+curl -sS https://isc.sans.edu/api/ipdetails/$ip_address | tr -d "{}" | jq 2>/dev/null > DH_report.txt
 #Check List 
-sleep 3
+echo "Checking If IP Was Thought To Be Malicious Among 42905+ IP's" 
+
+sleep 2
 echo "" 
-echo "Seeing If IP Was Thought To Be Malicious Among 42905+ IP's" 
 mal="grep -n "$ip_address" BlockIP.txt"
 if [[ -z "$mal" ]]; then
-  echo "Not Malicious" 
-  echo "Didn't Match Any Malicious IP On List" < MAL_REPORT.txt
+echo "Match Found !,Thought To Be Malicious"
 elif [[ -n "$mal" ]]; then
-  echo "Match Found !,Thought To Be Malicious"
-fi
+  echo "Not Malicious" 
+  echo "Didn't Match Any Malicious IP On List" > MAL_REPORT.txt
+  fi
+
 echo "OSINT Finished!" 
 echo " =========================" 
 echo "|==> Generating Report <==|"
 echo " =========================" 
-cat IPWHOIS_Report.txt MAL_REPORT.txt ANYGE_report.txt ROBTEX_REPORT.txt THREATCROWD_REPORT.txt > $ip_address-Report.txt
+cat IPWHOIS_Report.txt DH_report.txt MAL_REPORT.txt ANYGE_report.txt ROBTEX_REPORT.txt THREATCROWD_REPORT.txt > $ip_address-Report.txt
 rm -rf IPWHOIS_Report.txt MAL_REPORT.txt ANYGE_report.txt ROBTEX_REPORT.txt THREATCROWD_REPORT.txt
 echo -e "Output Should Be Saved To <IP address>.txt" 
 sleep 1.9
@@ -114,4 +126,4 @@ then
 else
    echo "Okay,You Can View It Another Time"
 fi 
-echo "If Something Went Wrong Please Consider Opening An Issue"
+echo "If Something Went Wrong Please Consider Opening An Issue Or If You Want To Recommended An API"
