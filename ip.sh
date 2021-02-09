15ff182e6db7bd0e081d53fcd005ceea1a01ac8a
Phone Metal <74254069+Phone-Metal@users.noreply.github.com>
2021-02-09 14:21:53 +0600

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -26,8 +26,9 @@ clear
 banner 
 	
 echo -e "\e[21mEnter IP Address:"
+echo "(Note: The Report Feature Is A Bit Buggy)"
 read ip_address 
-
+mkdir $ip_address-Report && cd $ip_address-Report
  
 ip_whois() {
 echo "Start Time : $(date) && Current Directory: $(pwd)" 
@@ -49,30 +50,46 @@ echo "Timezone : $(curl http://ipwhois.app/line/"$ip_address"?objects=timezone)"
 echo "Organisation : $(curl http://ipwhois.app/line/"$ip_address"?objects=org)" 
 echo "Amount of API calls for the current month : $(curl http://ipwhois.app/line/"$ip_address"?objects=completed_requests)"
 } 
-ip_whois
+
 echo "" 
 echo "Conducting Automated OSINT with ROBTEX FREE API:" 
 #workaround because I'm lazy 
 curl https://freeapi.robtex.com/ipquery/"$ip_address" | jq | tr -d "}{[]"  
- 
+curl https://freeapi.robtex.com/ipquery/"$ip_address" | jq | tr -d "}{[]" > ROBTEX_REPORT.txt
 echo "" 
 echo "Conducting Automated OSINT with THREAT CROWD" 
-curl https://www.threatcrowd.org/searchApi/v2/ip/report/?"$ip_address" | jq | tr -d "{}[]" 
+#curl https://www.threatcrowd.org/searchApi/v2/ip/report/?"$ip_address" | jq | tr -d "{}[]" 
+curl https://www.threatcrowd.org/searchApi/v2/ip/report/?ip=$ip_address | jq | tr -d "{}[]" 
+curl https://www.threatcrowd.org/searchApi/v2/ip/report/?ip=$ip_address | jq | tr -d "{}[]" > THREATCROWD_REPORT.txt
 echo "" 
 gvar="https://www.google.com/search?&ie=UTF-8&oe=UTF-8&q=intext:%$ip_address" 
 echo "Conducting Automated OSINT with Google" 
 echo "URL : $gvar" 
 echo "" 
+ip_whois
+ip_whois < IPWHOIS_Report.txt  
 echo "Conducting Automated OSINT with ANY.GE API" 
 curl https://any.ge/api/ip/api.php?"$ip_address" | tr -d "{}"
+curl https://any.ge/api/ip/api.php?"$ip_address" | tr -d "{}" > ANYGE_report.txt
 echo "" 
 echo "Seeing If IP Was Thought To Be Malicious Among 42905+ IP's" 
 mal="grep -n "$ip_address" BlockIP.txt"
 if [[ -z "$mal" ]]; then
-  echo "Not Malicious"
+  echo "Not Malicious" 
+  echo "Didn't Match Any Malicious IP On List" > MAL_REPORT.txt
 elif [[ -n "$mal" ]]; then
   echo "Match Found !,Thought To Be Malicious"
 fi
-
-##echo -e "\e[92mOutput Should Be Saved To Report_For-<IP Address>.txt" 
+echo "Generating Report" 
+cat IPWHOIS_Report.txt MAL_REPORT.txt ANYGE_report.txt ROBTEX_REPORT.txt THREATCROWD_REPORT.txt > $ip_address-Report.txt
+rm -rf IPWHOIS_Report.txt MAL_REPORT.txt ANYGE_report.txt ROBTEX_REPORT.txt THREATCROWD_REPORT.txt
+echo -e "\e[92mOutput Should Be Saved To <IP address>.txt" 
+echo "View Report Now?(y/n)" 
+read report
+if [ $report == y ]
+then
+   less $ip_address-Report.txt 
+else
+   echo "Okay,You Can View It Another Time"
+fi 
 echo "\e[1mIf Something Went Wrong Please Consider Opening An Issue"
